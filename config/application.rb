a1f1b3a90cc40ff66b5726d20b83c5295fd6fd1d
Lorena Bento <lorenadgb@gmail.com>
2017-02-12 21:42:38 -0200

    Set default locale to pt-BR

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module KeyControl
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = "pt-BR"
+
+    config.encoding = "utf-8"
   end
 end
