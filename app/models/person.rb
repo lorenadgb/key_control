a7d1e57df6c538b80999325cd7c85d8603cdcd42
Lorena Bento <lorenadgb@gmail.com>
2017-02-26 20:00:51 -0300

    Add scopes to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,4 +9,7 @@ class Person < ApplicationRecord
 
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
+
+  scope :visitors, -> { where { personable_type.eq PersonableType::VISITOR } }
+  scope :owners,   -> { where { personable_type.eq PersonableType::OWNER } }
 end
