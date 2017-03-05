f737a5d3510615980358aebb9966d5b0d74fbcfe
Lorena Bento <lorenadgb@gmail.com>
2017-03-05 15:49:36 -0300

    Redirect to new_real_state_agency after login if there is not a new_real_state_agency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    redirect_to :new_real_state_agency unless real_state_agency?
+  end
+
+  private
+
+  def real_state_agency?
+    RealStateAgency.last
+  end
+
 end
