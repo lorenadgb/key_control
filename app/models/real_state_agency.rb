59a5f6d7f2ae04646b0c685526e978a614abf366
Lorena Bento <lorenadgb@gmail.com>
2017-03-05 17:12:03 -0300

    Allow only one real state agency record

--- app/models/real_state_agency.rb
+++ app/models/real_state_agency.rb
@@ -1,8 +1,17 @@
 class RealStateAgency < ApplicationRecord
   extend EnumerateIt
 
+  before_create :only_one_row
+
   mount_uploader :avatar, AvatarUploader
 
   has_one :address, as: :addressable, dependent: :destroy
   accepts_nested_attributes_for :address
+
+  private
+
+  def only_one_row
+    false if RealStateAgency.count > 0
+  end
+
 end
