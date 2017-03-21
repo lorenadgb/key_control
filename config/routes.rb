918857bf3e3bdba43b3ae14a48c0f7f79ea90bfb
Lorena Bento <lorenadgb@gmail.com>
2017-03-20 21:48:26 -0300

    Remove unnecessary views from keys

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :real_state_agencies, except: :destroy
-  resources :keys, except: :new
+  resources :keys, except: [:new, :show, :edit]
   resources :addresses
   resources :people
   resources :buildings
