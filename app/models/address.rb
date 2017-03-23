af0e6e346b375ef0ec124c812da7d72a497f3110
Lorena Bento <lorenadgb@gmail.com>
2017-03-22 22:33:59 -0300

    Remove complement from validates of presence

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   extend EnumerateIt
 
-  validates :street_type, :name, :number, :complement, presence: true
+  validates :street_type, :name, :number, presence: true
 
   has_enumeration_for :street_type
 
