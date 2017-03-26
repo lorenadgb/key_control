605d440c7d08854983f5181001ea973e2b7c8980
Lorena Bento <lorenadgb@gmail.com>
2017-03-25 22:43:48 -0300

    Only available keys can be borrowed

--- app/models/key.rb
+++ app/models/key.rb
@@ -4,6 +4,8 @@ class Key < ApplicationRecord
 
   validates :code, presence: true
 
+  scope :availables, -> { where( status: KeyStatus::AVAILABLE ) }
+
   def update_key_status_to_borrowed
     update_column :status, KeyStatus::BORROWED
   end
