d41ac9ddc11173e2f9c5cf857ec3a89968767dd7
Lorena Bento <lorenadgb@gmail.com>
2017-04-11 20:06:16 -0300

    Fix flash color

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -38,13 +38,14 @@ class PeopleController < CrudController
   end
 
   def destroy
+    binding.pry
     if @person.destroy
     respond_to do |format|
       format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
       format.json { head :no_content }
     end
     else
-      flash[:notice] = 'Person can not be destroyed.'
+      flash[:error] = 'Person can not be destroyed.'
       redirect_to root_path
     end
   end
