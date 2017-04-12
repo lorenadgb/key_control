4a2ec32bfa19a24df64f0c41a7213f9ffe197fbd
Lorena Bento <lorenadgb@gmail.com>
2017-04-11 21:25:56 -0300

    Key: Add building available condition to scope

--- app/models/key.rb
+++ app/models/key.rb
@@ -7,7 +7,7 @@ class Key < ApplicationRecord
   scope :by_code, -> (code){ where( code: code ) }
   scope :by_building_id, -> (building_id){ where( building_id: building_id ) }
   scope :by_owner_id, -> (owner_id){ Key.joins(:building).where(buildings: {person_id: owner_id}) }
-  scope :availables, -> { where( status: KeyStatus::AVAILABLE ) }
+  scope :availables, -> { Key.joins(:building).where(buildings: {status: BuildingStatus::AVAILABLE}).where( status: KeyStatus::AVAILABLE ) }
   scope :actives, -> { Key.joins(:building).where(buildings: {active: true}).where(buildings: {status: BuildingStatus::AVAILABLE}) }
 
   def update_key_status_to_borrowed
