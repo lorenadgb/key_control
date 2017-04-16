c01e15d8b66a941373b9520a69be09eca21880d1
Lorena Bento <lorenadgb@gmail.com>
2017-04-16 20:58:07 -0300

    Add bin/wkhtmltopdf-amd64

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,9 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  :layout => "pdf.html",
-  :exe_path => '/usr/bin/wkhtmltopdf'
-}
+if Rails.env.production?
+  WickedPdf.config = { exe_path: "#{Rails.root}/bin/wkhtmltopdf-amd64", wkhtmltopdf: "#{Rails.root}/bin/wkhtmltopdf-amd64" }
+else
+  WickedPdf.config = {
+    #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+    :layout => "pdf.html",
+    :exe_path => '/usr/bin/wkhtmltopdf'
+  }
+end
