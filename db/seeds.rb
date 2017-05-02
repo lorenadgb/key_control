d77f6e2062ae7f4800ed9142f017c5c6990924e4
Lorena Bento <lorenadgb@gmail.com>
2017-05-02 19:14:15 -0300

    Add admin to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create(email: 'adm@adm.com', password: 'change123', password_confirmation: 'change123' )
+User.create(email: 'adm@adm.com', password: 'change123', password_confirmation: 'change123', admin: true)
 RealStateAgency.create(trade_name: '', cnpj: '99.999.999/9999-99')
