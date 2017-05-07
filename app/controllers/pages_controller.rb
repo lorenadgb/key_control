6a18a068241810c76fd95767b6ab3c68e03d03cf
Lorena Bento <lorenadgb@gmail.com>
2017-05-06 22:44:16 -0300

    WillPaginate: Fix pagination per tabs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < CrudController
 
   def index
-    @buildings = Building.paginate(page: params[:page])
-    @keys      = Key.paginate(page: params[:page]).order_by_source_and_code.actives
-    @people    = Person.paginate(page: params[:page])
-    @owners    = Person.owners.paginate(page: params[:page])
-    @visitors  = Person.visitors.paginate(page: params[:page])
-    @realtors  = Person.realtors.paginate(page: params[:page])
-    @visits    = Visit.paginate(page: params[:page])
+    @buildings = Building.paginate(page: params['building'])
+    @keys      = Key.paginate(page: params['key']).order_by_source_and_code.actives
+    @people    = Person.paginate(page: params['people'])
+    @owners    = Person.owners.paginate(page: params['owner'])
+    @visitors  = Person.visitors.paginate(page: params['visitor'])
+    @realtors  = Person.realtors.paginate(page: params['realtor'])
+    @visits    = Visit.paginate(page: params['visit'])
 
     @tab = params[:tab]
   end
