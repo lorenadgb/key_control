7ae9a1623e03a6dd7921b21043b4446da6e6e36c
Lorena Bento <lorenadgb@gmail.com>
2017-05-28 21:43:04 -0300

    Add validates to setting

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,8 @@
 class Setting < ApplicationRecord
 
   belongs_to :real_state_agency
+
+  validates :max_number_of_keys_per_type, presence: true
+  validates_numericality_of :max_number_of_keys_per_type, greater_than: 0
+
 end
